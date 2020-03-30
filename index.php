e7023947ad486ecb12b3c9962d99ba893dce997f
Fatroul <greensgoldberg@gmail.com>
2020-03-30 14:26:01 +0200

    avec accesseur et mutateur

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php 
 class Personnage {
-    public $nom;
-    public $force;
-    public $lvl; 
-    public $health;
+    private $nom;
+    private $force;
+    private $health;
+    private $lvl; 
+    
 
+    function __construct(string $nom, int $force, int $health, int $lvl = 1){
+        $this->nom = $nom;
+        $this->force = $force; 
+        $this->health = $health;
+        $this->lvl = $lvl;
+
+    }
 
     public function alive() {
     if($this->health > 0) {
@@ -17,30 +25,32 @@ class Personnage {
 
 
     public function caracteristiques() {
-echo "Le personnage $this->nom a une force de $this->force et est {$this->alive()}";
+echo "Le personnage ".$this->nom." a une force de ".$this->force." lvl ".$this->lvl." et est ".$this->alive();
     }
- 
 
+    function getNom() string {
+        return $this->nom;
+    }
+    function setNom(string $nom){
+        $this->nom = $nom;
+
+    }
+    function getLvl() int {
+        return $this->lvl;
+    }
+    function setNom(int $lvl){
+        $this->lvl = $lvl;
 }
 
-$perso1 = new Personnage();
-$perso1-> nom = "Rose";
-$perso1-> force = "12";
-$perso1-> lvl = "6";
-$perso1-> health = "100";
+$perso1 = new Personnage("Rose", 12, 100);
 
-$perso2 = new Personnage();
-$perso2-> nom = "Golbu";
-$perso2-> force = "15";
-$perso2-> lvl = "8";
-$perso2-> health = "100";
+$perso2 = new Personnage("Golbu", 15, 100, 2);
 
-$perso3 = new Personnage();
-$perso3-> nom = "Arthis";
-$perso3-> force = "13";
-$perso3-> lvl = "33";
-$perso3-> health = "100";
+$perso3 = new Personnage("Arthis", 13, 0, 2);
 
-$perso3->caracteristiques();
+
+$perso2->setNom("Jacquouille");
+echo $perso2->getNom(); 
+//$perso3->caracteristiques();
 
 ?>
